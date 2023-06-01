346377e0d24463549fdc5f8080b920980c680ab6
mairakoba <130098548+mairakoba@users.noreply.github.com>
2023-06-01 13:06:54 -0300

    Finalizado banner, modificado e excluído pastas

--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 }
+
+
+/***BANNER ROLAGEM***/
+let contador = 1;
+
+setInterval(function(){
+    document.getElementById('slide' + contador).checked = true;
+    contador ++;
+
+    if( contador > 4) {
+        contador = 1;
+    }
+
+}, 4000);
--- salads.js
+++
@@ -1,8 +0,0 @@
-let saladJson = [
-    {id:1, name:'Crocante', img:'images/crocante.svg', price:15.00, sizes:['150g', '250g', '350g'], description:'Alface americana, alface lisa, rúcula, repolho roxo, crottons e molho especial.'},
-    {id:2, name:'Mista', img:'images/mista.svg', price:20.00, sizes:['150g', '250g', '350g'], description:''},
-    {id:3, name:'Mix bacon', img:'images/bacon.svg', price:22.00, sizes:['150g', '250g', '350g'], description:''},
-    {id:4, name:'Tradicional', img:'images/tradicional.svg', price:15.00, sizes:['150g', '250g', '350g'], description:''},
-    {id:5, name:'Da casa', img:'images/dacasa.svg', price:18.00, sizes:['150g', '250g', '350g'], description:''},
-    {id:6, name:'Caprese', img:'images/caprese.svg', price:20.00, sizes:['150g', '250g', '350g'], description:''}
-];
